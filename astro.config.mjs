4b778a77bb4eaf51c40b90e0bfe4873a21f067eb
BenjaminULRICH <benjaminulrich67@gmail.com>
2025-10-28 09:28:08 +0100

    blurb

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-
-	site:'https://unphilosophedanslespace.github.io',
-
+	site: 'https://unphilosophedanslespace.github.io',
 });
